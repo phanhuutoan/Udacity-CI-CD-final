e6bf1ab5f024623599ea3bb1fb857defa69c91c3
phanhuutoan <phanhuutoan.dev@outlook.com>
2023-04-04 17:23:08 +0700

    update backend

--- backend/src/modules/domain/employees/commands/handlers/employee-updater.handler.ts
+++ backend/src/modules/domain/employees/commands/handlers/employee-updater.handler.ts
@@ -47,9 +47,7 @@ export class EmployeeUpdater extends BaseCommandHandler<UpdateEmployee, void> {
     employee.displayName = displayName;
     employee.companyEmail = companyEmail;
     employee.personalEmail = personalEmail;
-    employee.birthdate = moment(birthdate)
-      .utc()
-      .format();
+    employee.birthdate = moment(birthdate).toDate();
     employee.address = address;
     employee.phoneNumber = phoneNumber;
     employee.tags = tags;
@@ -58,9 +56,7 @@ export class EmployeeUpdater extends BaseCommandHandler<UpdateEmployee, void> {
     employee.city = city;
     employee.salary = +salary;
     employee.salaryType = SalaryType[salaryTypeKey];
-    employee.effectiveDate = moment(effectiveDate)
-      .utc()
-      .format();
+    employee.effectiveDate = moment(effectiveDate).toDate();
 
     await this.employeeRepository.save(employee);
   }
