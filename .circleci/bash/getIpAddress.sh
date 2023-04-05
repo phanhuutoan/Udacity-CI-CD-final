fcc3072a7897ecedf0c599a8e362467cf754adb2
phanhuutoan <phanhuutoan.dev@outlook.com>
2023-04-05 19:37:04 +0700

    test config

--- .circleci/bash/getIpAddress.sh
+++ .circleci/bash/getIpAddress.sh
@@ -1,4 +1,6 @@
 STACK_NAME=$1
 
+echo "The stack name $STACK_NAME"
+
 INSTACNE_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[0].OutputValue" --output text)
 IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $INSTACNE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
