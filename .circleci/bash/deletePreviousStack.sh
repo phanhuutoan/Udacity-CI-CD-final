9175b1daf13815010ce127d8043e778991c714ec
phanhuutoan <phanhuutoan.dev@outlook.com>
2023-04-08 10:44:39 +0700

    combine together

---
+++ .circleci/bash/deletePreviousStack.sh
@@ -0,0 +1,17 @@
+# Fetch the Old workflow ID
+export OldWorkflowID=$(aws cloudformation \
+          list-exports --query "Exports[?Name==\`WorkflowID\`].Value" \
+          --no-paginate --output text)
+echo OldWorkflowID: "${OldWorkflowID}"
+echo CIRCLE_WORKFLOW_ID "${CIRCLE_WORKFLOW_ID:0:7}"
+
+if [[ "${CIRCLE_WORKFLOW_ID:0:7}" != "${OldWorkflowID}" ]]
+then
+    echo "Start to clean up..."
+    aws s3 rm "s3://udapeople-${OldWorkflowID}" --recursive
+    aws cloudformation delete-stack --stack-name "udapeople-backend-${OldWorkflowID}"
+    aws cloudformation delete-stack --stack-name "udapeople-frontend-${OldWorkflowID}"
+    echo "Delete done"
+else
+    echo 'No need to clean any thing'
+fi
