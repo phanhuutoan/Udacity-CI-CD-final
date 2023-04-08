1491f2b822eaf5f3b928617ba65e8480d8c42276
phanhuutoan <phanhuutoan.dev@outlook.com>
2023-04-08 10:09:53 +0700

    combine together

--- .circleci/bash/feSmokeTest.sh
+++ .circleci/bash/feSmokeTest.sh
@@ -4,8 +4,7 @@ URL="http://udapeople-${ID:0:7}.s3-website-us-east-1.amazonaws.com/#/employees"
 echo ${URL} 
 if curl -s --connect-timeout 10 ${URL} | grep "Welcome"
 then
-	# Change this to 0 after the job fails
-  exit 1
+  exit 0
 else
   exit 1
 fi
